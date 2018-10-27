bf6f3b4d6ec9994ea4e75b7918a16383c04be01d
Noam <noamoss@gmail.com>
2018-10-27 21:55:32 +0300

    add error message if user location could not be share on mobile app page

--- mapapp/scripts/mobilemapapp.js
+++ mapapp/scripts/mobilemapapp.js
@@ -6,7 +6,7 @@ var mapbox_access_token = config.MAPBOX_ACCESS_TOKEN;
 
 
 // create a map instance
-var mymap = L.map('map').locate({setView: true, maxZoon: 16});
+var map = L.map('map').locate({setView: true, maxZoon: 16});
 
 // add layer to map
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
@@ -14,21 +14,24 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
     maxZoom: 18,
     id: 'mapbox.streets',
     accessToken: mapbox_access_token
-}).addTo(mymap);
+}).addTo(map);
 
 
-var marker = L.marker([51.5, -0.09]).addTo(mymap);
-marker.bindPopup("<b>הי חביבי!</b><br>אני פופ-אפ.").openPopup();
+function onLocationFound(e) {
+    var radius = e.accuracy / 2;
 
+    L.marker(e.latlng).addTo(map)
+        .bindPopup("אתם נמצאים ברדיוס של "+ radius + " מטרים מהנקודה הזו").openPopup();
+    L.circle(e.latlng, radius).addTo(map);
+}
 
+map.on('locationfound', onLocationFound);
 
-var circle = L.circle([51.508, -0.11], {
-    color: 'red',
-    fillColor: '#f03',
-    fillOpacity: 0.5,
-    radius: 500
-}).addTo(mymap);
+function onLocationError(e) {
+    alert("האפליקציה לא הצליחה לאתר את המיקום המדוייק שלך");
+}
 
+map.on('locationerror', onLocationError);
 
 
 function onMapClick(e) {
@@ -41,7 +44,7 @@ function onMapClick(e) {
   popup
     .setLatLng(e.latlng)
     .setContent("לחצת על המפה ב:" + e.latlng.toString())
-    .openOn(mymap);
+    .openOn(map);
 }
 
-mymap.on('click', onMapClick);
+map.on('click', onMapClick);
